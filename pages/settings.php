71eb65355b4824d9df05a7f6bb56c26f0e1879e9
MARK ARVIN DELA CRUZ <2232533@slu.edu.ph>
2024-10-21 14:58:58 +0000

    Update settings.php

--- pages/settings.php
+++ pages/settings.php
@@ -63,74 +63,68 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <title>Settings</title>
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
     <link rel="stylesheet" href="../style/settings.css" />
+    
 </head>
 <body>
     <div class="sidebar">
         <div class="sidebar-brand">
-            <div class="brand-flex">
-                <img class="logocircle" src="../assets/alumnilogo.png" width="30px" alt="" />
-                <div class="brand-icon">
-                    <span class="las la-bell"></span>
-                    <span class="las la-user-circle"></span>
-                </div>
+          <div class="brand-flex">
+            <div class="brand-icon">
+              <a href="javascript:void(0)" id="sidebarToggle">
+                <span
+                  ><img src="../assets/bars1.png" width="24px" alt="photo"/></span>
+              </a>
             </div>
+            <img
+              class="logocircle"
+              src="../assets/alumnilogo.png"
+              width="30px"
+              alt=""
+            />
+          </div>
         </div>
-        <div class="sidebar-main">
-            <div class="sidebar-user">
-                <img src="../assets/profile.jpg" alt="{PROFILE_ALT_TEXT}" />
-                <div>
-                    <h3><?= htmlspecialchars($user['first_name'] . ' ' . $user['last_name']) ?></h3>
-                    <span><?= htmlspecialchars($user['email']) ?></span>
-                </div>
+        <div class="sidebar-content">
+          <div class="sidebar-user">
+            <a href="../pages/viewProfile.php">
+              <img src="../assets/profile.jpg" alt="Profile Picture" />
+            </a>
+            <div>
+              <h3>
+                <?php echo htmlspecialchars($user['first_name'] . ' ' . $user['last_name']); ?>
+              </h3>
+              <span><?php echo htmlspecialchars($user['email']); ?></span>
             </div>
-            <div class="sidebar-menu">
-                <div class="menu-head">
-                    <span>DASHBOARD</span>
-                </div>
-                <ul>
-                    <li>
-                        <a href="{CALENDAR_LINK}">
-                            <span class="las la-calendar"></span>
-                            Calendar
-                        </a>
-                    </li>
-                    <li>
-                        <a href="{CONTACT_LINK}">
-                            <span class="las la-phone"></span>
-                            Contact
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../pages/events.html">
-                            <span class="las la-sign"></span>
-                            Events
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../pages/shareExperience.php">
-                            <span class="las la-image"></span>
-                            Share
-                        </a>
-                    </li>
-                    <li>
-                        <a href="../pages/settings.php">
-                            <span class="las la-tools"></span>
-                            Settings
-                        </a>
-                    </li>
-                </ul>
+          </div>
+          <div class="sidebar-menu">
+            <div class="menu-head">
+              <span>Dashboard</span>
             </div>
+            <ul>
+              <li>
+                <a href="../pages/shareExperience.php">
+                  <span><img src="../assets/home1.png" width="20px" alt="Home"/></span>Home</a>
+              </li>
+              <li>
+                <a href="../pages/events.php"
+                  ><span><img src="../assets/event1.png" width="20px" alt="Events" /></span>Events</a
+                >
+              </li>
+              <li>
+                <a href="../pages/settings.php"
+                  ><span><img src="../assets/setting1.png" width="20px" alt="Settings" /></span>Settings</a
+                >
+              </li>
+              <li>
+                <a href="../pages/loginpage.php"
+                  ><span><img src="../assets/logout1.png" width="20px" alt="Logout" /></span>Logout</a
+                >
+              </li>
+            </ul>
+          </div>
         </div>
-    </div>
+      </div>
 
     <div class="main-content">
-        <header>
-            <span class="las la-bars"></span>
-            <div class="header-icons">
-                <span class="las la-search"></span>
-            </div>
-        </header>
-        
         <main>
             <div class="page-header">
                 <div>
