edb60211c55830265a8f85938b7baed696a23427
Julianne Abitan <julianneabitan@gmail.com>
2024-12-01 17:06:40 +0800

    fixed right-panel - search results page

--- pages/alumni/searchResultsPage.php
+++ pages/alumni/searchResultsPage.php
@@ -47,6 +47,7 @@ if ($query) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Search Results</title>
     <link rel="stylesheet" href="../../style/searchResults.css" />
+    <link rel="stylesheet" href="../../style/friends-panel.css" />
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
 </head>
 
