0fa0f120dc05beb9a04082506e320f406779671d
Jan Dolby Aquino <2233400@slu.edu.ph>
2024-12-10 23:12:18 +0800

    Added Additional Statistics Query for Events

--- config/adminAndManager/controllers/eventStatisticsController.js
+++ config/adminAndManager/controllers/eventStatisticsController.js
@@ -9,3 +9,17 @@ exports.getTotalEvents = (req, res) => {
         res.json({ totalEvents: results[0].total_events });
     });
 };
+
+exports.getEventsGroupedByStatus = (req, res) => {
+    const sql = `
+        SELECT status, COUNT(*) AS total
+        FROM events
+        GROUP BY status;
+    `;
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Database error: ' + err.message });
+        }
+        res.json(results);
+    });
+};
