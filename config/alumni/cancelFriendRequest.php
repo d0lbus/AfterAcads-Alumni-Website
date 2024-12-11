7809ecbc584efe540c4c1aa24307d478fd6738a1
Julianne Abitan <julianneabitan@gmail.com>
2024-12-12 00:34:43 +0800

    citations - cancelFrienRequest.php

--- config/alumni/cancelFriendRequest.php
+++ config/alumni/cancelFriendRequest.php
@@ -1,3 +1,10 @@
+// References:
+// 1. W3Schools - For PHP basics, MySQL integration, and header redirection. (https://www.w3schools.com/php/php_mysql_intro.asp)
+// 2. StackOverflow - For input validation and handling redirection. (https://stackoverflow.com/questions/5637279/how-to-redirect-in-php)
+// 3. GeeksforGeeks - For error handling and general PHP tips. (https://www.geeksforgeeks.org/how-to-handle-errors-in-php/)
+// 4. YouTube - For video tutorials on PHP and MySQL basics. (Search "PHP MySQL tutorial" on YouTube)
+// 5. ChatGPT - For generating and explaining general PHP concepts and best practices.
+
 <?php
 include 'header.php';
 include '../../config/general/connection.php';
