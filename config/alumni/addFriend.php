9f8bf136a9b6613984d0f756f540511967ac9afd
Julianne Abitan <julianneabitan@gmail.com>
2024-12-12 00:33:55 +0800

    citations - addFriend.php

--- config/alumni/addFriend.php
+++ config/alumni/addFriend.php
@@ -1,3 +1,11 @@
+// References:
+// 1. W3Schools - For PHP basics, MySQL integration, and prepared statements. (https://www.w3schools.com/php/php_mysql_intro.asp)
+// 2. StackOverflow - For input validation and error handling. (https://stackoverflow.com/questions/6031904/how-to-validate-and-filter-user-input-in-php)
+// 3. GeeksforGeeks - For error handling and general PHP tips. (https://www.geeksforgeeks.org/how-to-handle-errors-in-php/)
+// 4. YouTube - For video tutorials on PHP and MySQL basics. (Search "PHP MySQL tutorial" on YouTube)
+// 5. ChatGPT - For generating and explaining general PHP concepts and best practices.
+// 6. PHP Manual - For details on the `json_encode` function. (https://www.php.net/manual/en/function.json-encode.php)
+
 <?php
 include 'header.php';
 include '../config/general/connection.php';
